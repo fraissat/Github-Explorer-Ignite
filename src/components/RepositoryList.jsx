97336d5e377e84a7e1531533239dbcf5c5700164
FFraissat <fabio.fraissat@ufu.br>
2021-04-14 19:54:15 -0300

    Módulo 3

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,12 +1,13 @@
 import { RepositoryItem } from "./RepositoryItem";
 
+import '../styles/repositories.scss'
+
 const repository = {
     name: 'unform',
     description : "Forms in React",
     link : "https://github.com/unform/unform"
 
-};
-
+}
 
 export function RepositoryList() {
     return (
